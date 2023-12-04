24a01418a55afc2b5ea669bc9d68f0308db51468
rbhor <rohan.bhor@tradingtechnologies.com>
2023-12-04 08:54:37 +0530

    publish website

--- publishing_website/streamlit_demo.py
+++ publishing_website/streamlit_demo.py
@@ -1,6 +1,16 @@
+import pandas
 import streamlit as st
 
+data = {
+    'series_1': [1, 3, 4, 5, 7],
+    'series_2': [10, 30, 40, 100, 250]
+}
+df = pandas.DataFrame(data)
 st.title('Our first Streamlit App')
-
 st.subheader('Introducing Streamlit in Automate Everything with Python')
 st.write('This is our first Web App')
+st.write(df)
+st.line_chart(df)
+
+my_slider = st.slider("Celcius")
+st.write(my_slider, 'In Fahrenheit is ', my_slider*9/5 + 32)
